f955d2441a6ed621e86c17223a1893206291e247
Eliseo Derghazarian <eliseoderghazarian@MacBook-Pro-de-Eliseo.local>
2021-05-30 21:01:29 -0300

    First commit

---
+++ Basic Swift.playground/Pages/From Variables to Dictionaries.xcplaygroundpage/Contents.swift
@@ -0,0 +1,69 @@
+import UIKit
+
+//*** VARIABLES AND CONSTANTS ***
+
+//Option 1
+var name: String
+name = "Eliseo"
+
+//Option 2
+var surname: String = "Derghazarian"
+let nationality : String = "Argentinian"
+
+
+
+//*** SOME DATA TYPES ***
+var age: Int = 29
+let height: Float = 1.70
+var city: String = "Buenos Aires"
+var latitude: Double = -34.603722
+var single: Bool = true
+
+
+
+//*** OPERATORS ***
+var number1 = 10
+var number2 = 5
+
+var addition = number1 + number2
+var sustraction = number1 - number2
+var multiplication = number1 * number2
+var division = number1 / number2
+var modulus = number1 % 2
+
+
+
+//*** STRING INTERPOLATION ***
+var presentation = "My name is \(name), I'm \(age) and I live in \(city)."
+
+
+
+//*** ARRAYS ***
+
+//Option 1
+var evenNumbers = [2, 4, 6, 8]
+
+//Option2
+var oddNumbers: [Int] = [1, 3, 5, 7]
+
+//Option 3
+var things: [Any] = ["Sun", "Moon", 42]
+
+//Option 4
+var songs: [String] = []
+
+//Option 5
+var books = [String]()
+
+
+
+//*** DICTIONARIES ***
+var myFamily = [
+                1: "Dad",
+                2: "Mum",
+                3: "Me",
+                4: "Sister",
+                5: "Brother"
+            ]
+
+
---
+++ Basic Swift.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,87 @@
+import UIKit
+
+//*** CONDITIONALS ***
+
+//Example 1
+var houseIsClean = true
+
+
+if houseIsClean {
+    print("Just tidy up the house.")
+} else {
+    print("Clean and tidy up the house.")
+}
+
+
+//Example 2
+let examScore = Int.random(in: 0...100)
+
+if examScore >= 80 {
+    print("You passed without having to take another exam. Congratz!")
+} else if examScore >= 60 && examScore < 80 {
+    print("You passed the exam. Still one exam remaining")
+} else {
+    print("You have not approved.")
+}
+
+
+
+//*** LOOPS ***
+
+//Example 1
+for i in 1...10 {
+    print("\(i) x 10 is \(i * 10)")
+}
+
+
+//Example 2
+var animals = ["dog", "cat", "bird"]
+var actions = ["barks", "meows", "sings" ]
+
+for i in 0..<animals.count {
+    print("A \(animals[i])" + " \(actions[i])")
+    
+}
+
+
+
+//*** SWITCH ***
+switch examScore {
+case 80...100:
+    print("You passed without having to take another exam. Congratz!")
+case 61...79:
+    print("You passed the exam. Still one exam remaining")
+default:
+    print("You have not approved.")
+}
+
+
+
+//*** FUNCTIONS ***
+
+//Option 1
+func countLettersInString(myString str: String) {
+    print("The string \(str) has \(str.count) letters.")
+}
+
+countLettersInString(myString: "Hello")
+
+
+//Option 2
+func countLettersInString(_ str: String) {
+    print("The string \(str) has \(str.count) letters.")
+}
+
+countLettersInString("Hello")
+
+
+//Option 3
+func countLetters(in string: String) {
+    print("The string \(string) has \(string.count) letters.")
+}
+
+countLetters(in: "Hello")
+
+
+
+
